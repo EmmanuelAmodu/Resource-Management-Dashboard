7c2084d7d0482833548a90cc80e7438452f1a887
Amodu Emmanuel <aWX536413@china.huawei.com>
2019-03-28 19:42:47 +0100

    question improvement

--- src/app/Modules/dynamic-forms/Class/question-base.ts
+++ src/app/Modules/dynamic-forms/Class/question-base.ts
@@ -1,7 +1,9 @@
 export class QuestionBase<T> {
     value: T;
     key: string;
+    type: String;
     label: string;
+    options: {};
     required: boolean;
     order: number;
     controlType: string;
@@ -12,11 +14,15 @@ export class QuestionBase<T> {
         label?: string,
         required?: boolean,
         order?: number,
+        type?: String,
+        options?: string,
         controlType?: string
     } = {}) {
         this.value = options.value;
         this.key = options.key || '';
         this.label = options.label || '';
+        this.type = options.type || '';
+        this.options = options.options || '';
         this.required = !!options.required;
         this.order = options.order === undefined ? 1 : options.order;
         this.controlType = options.controlType || '';
--- src/app/Modules/dynamic-forms/service/question/question.service.ts
+++ src/app/Modules/dynamic-forms/service/question/question.service.ts
@@ -7,37 +7,56 @@ import { TextboxQuestion } from '../../Class/question-textbox';
     providedIn: 'root'
 })
 export class QuestionService {
+    private typeMap = {'Dropdown': DropdownQuestion, 'Textbox': TextboxQuestion};
 
     // TODO: get from a remote source of question metadata
     // TODO: make asynchronous
     public getQuestions() {
-        const questions: QuestionBase<any>[] = [
-            new DropdownQuestion({
-                key: 'brave',
-                label: 'Bravery Rating',
-                options: [
-                    { key: 'solid', value: 'Solid' },
-                    { key: 'great', value: 'Great' },
-                    { key: 'good', value: 'Good' },
-                    { key: 'unproven', value: 'Unproven' }
-                ],
+        const questionRaw: any = [
+            {
+                key: 'product_type',
+                label: 'Product Type',
+                ftype: 'Dropdown',
+                options_model: 'product_type',
                 order: 3
-            }),
-            new TextboxQuestion({
+            },
+            {
                 key: 'firstName',
                 label: 'First name',
                 value: 'Bombasto',
+                ftype: 'Textbox',
                 required: true,
                 order: 1
-            }),
-            new TextboxQuestion({
+            },
+            {
                 key: 'emailAddress',
                 label: 'Email',
                 type: 'email',
+                ftype: 'Textbox',
                 order: 2
-            })
+            }
         ];
 
+        const questions = questionRaw.map(question => {
+            if (question.options_model) {
+                question.options = this.getOptions(question.options_model);
+                delete question.options_model;
+            }
+            const optionType = this.typeMap[question['ftype']];
+            delete question.options_model;
+            question = new optionType(question);
+            return question;
+        });
+
         return questions.sort((a, b) => a.order - b.order);
+    }
+
+    private getOptions(model) {
+        return [
+            { key: 'solid', value: 'Solid' },
+            { key: 'great', value: 'Great' },
+            { key: 'good', value: 'Good' },
+            { key: 'unproven', value: 'Unproven' }
+        ];
     }
 }
