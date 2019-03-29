feb1318995e62180e5713fdf9f467ddaa310ded2
Amodu Emmanuel <aWX536413@china.huawei.com>
2019-03-29 11:31:10 +0100

    commit before low format

--- src/app/Services/DataService/data.stub.temp.ts
+++ src/app/Services/DataService/data.stub.temp.ts
@@ -40,3 +40,39 @@ export class DataStub {
         return this.forms[name];
     }
 }
+
+let dft = {
+    form_name: 'SupplyRequestForm',
+    role: 'StationManager',
+    form_fields: [
+        {
+            key: 'product_type',
+            label: 'Product Type',
+            ftype: 'Dropdown',
+            options_model: 'product_type',
+            order: 1
+        },
+        {
+            key: 'station',
+            label: 'Filling Station',
+            value: 'Ketu Station',
+            ftype: 'Textbox',
+            required: true,
+            order: 2
+        },
+        {
+            key: 'quantity',
+            label: 'Quantity',
+            type: 'number',
+            ftype: 'Textbox',
+            order: 3
+        },
+        {
+            key: 'emailAddress',
+            label: 'Email',
+            type: 'email',
+            ftype: 'Textbox',
+            order: 4
+        }
+    ]
+}
