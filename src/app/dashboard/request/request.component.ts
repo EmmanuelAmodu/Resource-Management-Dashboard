e1d12b513aad0d2b084d5f1b8a3b3c9fc1bbf6e4
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2019-04-18 10:02:29 +0100

    make data an input property of the component

--- src/app/dashboard/request/request.component.ts
+++ src/app/dashboard/request/request.component.ts
@@ -8,12 +8,33 @@ import { DataService } from 'src/app/Services/DataService/data.service';
 })
 export class RequestComponent implements OnInit {
 	public form_name = 'SupplyRequestForm';
+	public gridHeader = {
+		'_id': 'ID',
+		'product_type': 'Product Type',
+		'station': 'Station',
+		'quantity': 'Quantity',
+		'emailAddress': 'Email Address',
+		'status': 'Status'
+	};
+
+	public pendingRequest: any[];
 
 	constructor(
 		private ds: DataService
 	) { }
 
 	ngOnInit() {
+		// TODO fetch from remote
+		this.pendingRequest = [
+			{
+				'_id' : 'REQ-20190417-51839',
+				'product_type' : 'great',
+				'station' : 'Ketu Station',
+				'quantity' : '12',
+				'emailAddress' : 'emmanuel.amodu@outlook.com',
+				'status' : 'initiated'
+			}
+		];
 	}
 
 	public submitFormAction(formData: {data_model: string, data: any}) {
